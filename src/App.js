74ce1aac1823cd1207eb586f63b9a7e262ef2f8b
Minh Tri <lieutri@gmail.com>
2021-02-02 10:46:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Navigation";
+import React from "react";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
-
+      <Content />
     </div>
   );
 }
