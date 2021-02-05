36231246afd3b3a2b9869c67d48a96f81a6b34af
Minh Tri <lieutri@gmail.com>
2021-02-05 20:40:39 +0700

    init"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-import Navigation from "./components/Navigation";
 import React from "react";
 import Content from "./components/Content";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import 'jquery-easing/dist/jquery.easing.1.3.umd'
+import 'jquery/dist/jquery.min'
+import './css/fonts.css'
+import './css/fonts2.css'
+import './css/styles.css'
+import './js/scripts'
 
 function App() {
   return (
     <div className="App">
-      <Content />
+      <Content/>
     </div>
   );
 }
